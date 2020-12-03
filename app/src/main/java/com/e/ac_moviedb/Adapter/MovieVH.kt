e73bd03f4893bb730bad2da46b17f1c8f1dfe6c0
0scarL <o_lizarazob@hotmail.com>
2020-12-03 08:28:30 -0500

    merge con develop branch

---
+++ app/src/main/java/com/e/ac_moviedb/Adapter/MovieVH.kt
@@ -0,0 +1,20 @@
+package com.e.ac_moviedb.Adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.e.ac_moviedb.Model.Movie
+import com.e.ac_moviedb.databinding.LayoutMovieItemBinding
+
+class MovieVH(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    private val binding = LayoutMovieItemBinding.bind(itemView)
+    fun bind (movie: Movie, movieListener: (movie: Movie)->Unit){
+        binding.itemTitle.text = movie.title
+        Glide.with(itemView.context)
+                .load("https://image.tmdb.org/t/p/w185/${movie.posterPath}")
+                .centerCrop()
+                .into(binding.itemImage)
+        itemView.setOnClickListener { movieListener(movie) }
+
+    }
+}
